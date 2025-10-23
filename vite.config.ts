d5dd66794e58cc178423779e7cc7389d82c871a7
hoan301298 <hoantran301298@gmail.com>
2025-10-23 13:55:24 +0300

    re-add resolve for path @

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/New-portfolio/', // 👈 must match your GitHub repo name exactly
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // 👈 this tells Vite what "@" means
+    },
+  },
 })
